5dd2edffd52d26cab1a5a2554d0c4b2a1b15b619
JP Cartailler <jp.cartailler@gmail.com>
2021-08-19 17:45:09 -0500

    added more docs and commented out test code

--- getBiomartAnnotations.R
+++ getBiomartAnnotations.R
@@ -1,12 +1,12 @@
 
-#load data----
+# load data----
 load("DATA/Islets2.Rda")
 
-#Extract Gene description
+# extract Gene description
 ref<-read_tsv("DATA/refdata-cellranger-GRCh38-1.2.0/Homo_sapiens.GRCh38.84_gene_annotation_table.txt")
 
 
-#fetch gene id info from biomart
+# fetch gene id info from biomart
 library(biomaRt)
 mart <- useEnsembl(biomart = "ensembl", 
                    host = "uswest.ensembl.org",
@@ -19,15 +19,18 @@ genemap <- getBM( attributes = c("ensembl_gene_id",'hgnc_symbol', "description",
                   mart = mart,
                   useCache = FALSE)
 
-r <- setdiff(ref$gene_id, genemap$ensembl_gene_id) # what is in A that isn't in B, setdiff(A, B)       
-head(r)
+#r <- setdiff(ref$gene_id, genemap$ensembl_gene_id) # what is in A that isn't in B, setdiff(A, B)       
+#head(r)
 
 # bind the seurat data back to original GRC38 genes, NOT biomart
 idx <- match( rownames(Islets), ref$GeneSymbol)
 Islets_ensembl <- ref[ idx, ]
 
+# now, bind the biomart data
 idx2 <- match( Islets_ensembl$gene_id, genemap$ensembl_gene_id) 
 Islets_ensembl <- genemap[ idx, ]
 
-# Fill in empty symbols
+# fill in empty symbols
 Islets_ensembl$hgnc_symbol <- ifelse(Islets_ensembl$hgnc_symbol == "", "No symbol available", Islets_ensembl$hgnc_symbol)
+
+# and save... as you'd like
